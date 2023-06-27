848c4ba3e25333723f3dfde98c1ecd97aa94e821
Baruch Giverts <bor770@gmail.com>
2023-06-27 11:48:33 +0300

    installed ngrx

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,12 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, isDevMode } from '@angular/core';
 import {
   PreloadAllModules,
   provideRouter,
   withPreloading,
 } from '@angular/router';
+import { provideRouterStore } from '@ngrx/router-store';
+import { provideStore } from '@ngrx/store';
+import { provideStoreDevtools } from '@ngrx/store-devtools';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withPreloading(PreloadAllModules))],
+  providers: [
+    provideRouter(routes, withPreloading(PreloadAllModules)),
+    provideRouterStore(),
+    provideStore(),
+    provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }),
+  ],
 };
