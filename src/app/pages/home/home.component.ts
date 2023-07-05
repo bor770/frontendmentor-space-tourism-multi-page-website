2f7ddf9b254cffa4459c691245568eb631b4d4c6
Baruch Giverts <bor770@gmail.com>
2023-07-05 12:21:21 +0300

    almost finished mobile home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LetDirective } from '@ngrx/component';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Width } from '../../shared/layout/layout.model';
+import * as fromRoot from '../../store/root.reducer';
 
 @Component({
+  imports: [CommonModule, LetDirective, RouterModule],
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./home.component.css'],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  width$: Observable<Width>;
 
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.width$ = this.store.select(fromRoot.selectLayoutWidth);
+  }
 }
--- src/app/shared/navigation/navigation.component.ts
+++ src/app/shared/navigation/navigation.component.ts
@@ -1,13 +1,25 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { LetDirective } from '@ngrx/component';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import { Width } from '../layout/layout.model';
+import * as fromRoot from '../../store/root.reducer';
 
 @Component({
+  imports: [CommonModule, LetDirective],
   selector: 'app-navigation',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: ['./navigation.component.css'],
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.css']
 })
-export class NavigationComponent {
+export class NavigationComponent implements OnInit {
+  width$: Observable<Width>;
 
+  constructor(private store: Store) {}
+
+  ngOnInit(): void {
+    this.width$ = this.store.select(fromRoot.selectLayoutWidth);
+  }
 }
