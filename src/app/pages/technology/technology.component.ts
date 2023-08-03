ae34c363206d05e76299c34f8a1e4a5eab92245e
Baruch Giverts <bor770@gmail.com>
2023-08-03 12:03:23 +0300

    mobile technology

--- src/app/pages/technology/technology.component.ts
+++ src/app/pages/technology/technology.component.ts
@@ -1,13 +1,24 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LetDirective } from '@ngrx/component';
+
+import { BaseDataPageComponent } from '../../shared/base-pages/base-data-page/base-data-page.component';
 
 @Component({
+  imports: [CommonModule, LetDirective, RouterModule],
   selector: 'app-technology',
   standalone: true,
-  imports: [CommonModule],
+  styleUrls: [
+    `../../shared/styles/page.css`,
+    `../../shared/styles/mobile.page.css`,
+    `../../shared/styles/tablet.page.css`,
+    `../../shared/styles/desktop.page.css`,
+    './styles/technology.component.css',
+    './styles/mobile.technology.component.css',
+    './styles/tablet.technology.component.css',
+    './styles/desktop.technology.component.css',
+  ],
   templateUrl: './technology.component.html',
-  styleUrls: ['./technology.component.css']
 })
-export class TechnologyComponent {
-
-}
+export class TechnologyComponent extends BaseDataPageComponent {}
